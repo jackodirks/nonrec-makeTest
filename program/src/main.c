8c1d998f50031ea0fbea6e4fd690b433f01d5c41
Jacko Dirks <jacko.dirks@gmail.com>
2014-07-21 19:25:48 +0200

    Added stuff

---
+++ program/src/main.c
@@ -0,0 +1,16 @@
+#include "vw.h"
+#include "ferrari.h"
+#include "volvo.h"
+#include "sheep.h"
+#include "cow.h"
+#include "horse.h"
+
+int main(int argc, char** argv){
+    printVw();
+    printFerrari();
+    printVolvo();
+    printSheep();
+    printCow();
+    printHorse();
+    return 0;
+}
